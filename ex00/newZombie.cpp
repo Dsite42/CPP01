7094990f3ddd0ec2a0b96e06b1ffb0eb4d2022a6
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-09-21 21:49:34 +0200

    ex00 implemented

--- ex00/newZombie.cpp
+++ ex00/newZombie.cpp
@@ -6,7 +6,16 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:43:27 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/09/21 15:43:28 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/09/21 21:12:16 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie *newZombie( std::string name )
+{
+	Zombie *newZombie;
+	
+	newZombie = new Zombie(name);
+	return (newZombie);
+}
--- ex00/randomChump.cpp
+++ ex00/randomChump.cpp
@@ -6,7 +6,14 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:44:21 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/09/21 15:44:24 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/09/21 21:38:10 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+void randomChump(std::string name)
+{
+	Zombie randomChump(name);
+	randomChump.announce();
+}
