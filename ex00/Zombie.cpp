3a9a6f5c6e95a0ae328c9916a4924cd4083e712e
cgodecke <cgodecke@student.42wolfsburg.de>
2023-09-22 09:42:45 +0200

    added quotation marks at constructor and deconstructor message

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 15:43:16 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/09/21 21:35:36 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/09/22 09:38:56 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 Zombie::Zombie(std::string name)
 {
 	this->_name = name;
-	std::cout << "Zombie " << _name << " created" << std::endl;
+	std::cout << "Zombie \"" << _name << "\" created" << std::endl;
 }
 
 Zombie::~Zombie()
 {
-	std::cout << "Zombie " << _name << " destroyed" << std::endl;
+	std::cout << "Zombie \"" << _name << "\" destroyed" << std::endl;
 }
 
 void Zombie::announce()
