3d80c059e8b55b78ae51fa91e63b0ca48970ac7b
cgodecke <cgodecke@student.42wolfsburg.de>
2023-10-24 15:55:36 +0200

    ex06: fixed segfault if no argument is given.

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 11:23:38 by cgodecke          #+#    #+#             */
-/*   Updated: 2023/10/04 17:56:22 by cgodecke         ###   ########.fr       */
+/*   Updated: 2023/10/24 15:51:09 by cgodecke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ int main (int argc, char **argv)
 	std::string level;
 	Harl harl;
 	
-	level = argv[1];
 	if (argc != 2)
 	{
 		std::cout << "Wrong number of arguments!\n";
 		return (0);
 	}
+	level = argv[1];
 	harl.complain(level);
 	return (0);
 }
